365a7539f8d2218dc063d5d74d993bf7acf71d4e
Raul Migliari <raulmigliari5@gmail.com>
2025-05-24 11:46:25 -0300

    Criando novos componentes

--- react-fundamentals/src/App.jsx
+++ react-fundamentals/src/App.jsx
@@ -1,14 +1,14 @@
-function sum (a,b) {
-  return a + b
-}
+import Title from './components/Title'
+import Subtitle from './components/Subtitle'
+import StatusText from './components/StatusText'
 
 export default function App() {
-  const tech = "React"
 
   return (
     <div className="App">
-      <h1>{tech} is awesome!</h1>
-      <h2>It's easy, like 1 + 2 is {sum(1,2)}</h2>
+      <Title />
+      <Subtitle />
+      <StatusText />
     </div>
   )
 }
---
+++ react-fundamentals/src/components/Subtitle.jsx
@@ -0,0 +1,7 @@
+function sum(a,b) {
+    return a + b
+}
+
+export default function Subtitle() {
+    return <h2>It's easy, like 1 + 2 is {sum(1,2)}</h2>
+}
