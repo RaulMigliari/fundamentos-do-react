0d88df2804b1c90485df339a2a705a1662e3fc60
Raul Migliari <raulmigliari5@gmail.com>
2025-05-25 17:35:09 -0300

    Contrando inputs usando state

--- ex4-gerador-de-senhas/src/App.jsx
+++ ex4-gerador-de-senhas/src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import { use, useState } from 'react'
 import './styles/globals.css'
 
 export default function App() {
   const [password, setPassword] = useState('')
   const [textButtonCopy, setTextButtonCopy] = useState('Copiar')
+  const [passwordSize, setPasswordSize] = useState(12)
 
   function generatePassword() {
     const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let length = 12
     let newPassword = ''
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < passwordSize; i++) {
       const indiceAleatorio = Math.floor(Math.random() * caracteres.length);
       newPassword += caracteres[indiceAleatorio];
     }
@@ -20,6 +20,16 @@ export default function App() {
   return (
     <div className='app'>
       <h1>Gerador de senhas</h1>
+      <div className='input'>
+        <label htmlFor="passwordSize">Tamanho:</label>
+        <input
+          type="number"
+          id='passwordSize'
+          min={1}
+          value={passwordSize}
+          onChange={(ev) => {setPasswordSize(ev.target.value)}}
+        />
+      </div>
       <div>
         <button onClick={generatePassword}>Gerar!</button>
         <button 
